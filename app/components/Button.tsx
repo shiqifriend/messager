cd9aa547931e655561bfea96b2aee049227b4be5
十七 <10858187+shiqi547git@user.noreply.gitee.com>
2023-05-26 21:24:30 +0800

    init: 项目登录界面完成

---
+++ app/components/Button.tsx
@@ -0,0 +1,42 @@
+'use client';
+
+import clsx from 'clsx';
+
+interface ButtonProps {
+  type?: 'button' | 'submit' | 'reset' | undefined;
+  fullWidth?: boolean;
+  children?: React.ReactNode;
+  onClick?: () => void;
+  secondary?: boolean;
+  danger?: boolean;
+  disable?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  type,
+  fullWidth,
+  children,
+  onClick,
+  secondary,
+  danger,
+  disable,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disable}
+      className={clsx(
+        `flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offet-2`,
+        disable && 'opacity-50 cursor-default',
+        fullWidth && 'w-full',
+        secondary? 'text-gray-900' : 'text-white',
+        danger && 'bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600',
+        !secondary && !danger && 'bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600'
+      )}
+    >{children}
+    </button>
+  );
+};
+
+export default Button;
